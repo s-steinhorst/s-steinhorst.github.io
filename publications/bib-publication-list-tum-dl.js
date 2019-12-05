6315db46e79467065f7879b2ab1339dcb2944334
Jan Lauinger <jan.lauinger@tum.de>
2019-12-05 18:54:38 +0100

    non working biblinks

--- publications/bib-publication-list-tum-dl.js
+++ publications/bib-publication-list-tum-dl.js
@@ -91,7 +91,7 @@ var bibtexify = (function($) {
         // adds the bibtex link and the opening div with bibtex content
         bibtex: function(entryData) {
             var itemStr = '';
-            itemStr += ' <a onclick="window.open(document.location.url,\'_blank\').document.write(\'';
+            itemStr += ' <a href="#" title="This article as BibTeX" target="_blank" onclick="window.open(document.location.url,\'_blank\').document.write(\'';
 			itemStr += '@' + entryData.entryType + "{" + entryData.cite + ",\n";
             $.each(entryData, function(key, value) {
                 if (key == 'author') {
